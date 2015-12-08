56a538c8f5e315387bf12d9641f3eec88c78b0b7
karparker <parker.karissa@gmail.com>
2015-12-08 16:49:44 -0500

    Commit portfolio

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,17 +1,13 @@
 $(function(){
-	$('.bell').on('click', function(){
-		$('.alert').addClass('open');
-	});
-	$('.close').on('click', function(){
-		$('.alert').removeClass('open');
-	});
-});
-$(function(){
-	console.log('working')
-	$('.table2').on('mouseover', function(){
+	console.log('wprking')
+	$('h1').on('mouseover', function(){
 		$(this).toggleClass('class1');
-	});
-	$('.table2').on('mouseleave', function(){
+	$('h1').on('mouseleave', function(){
 		$(this).removeClass('class1');
 	});
-});
+	$("divphotos").on('mouseover', function(){
+		$(this).animate('bounce');
+	});
+	
+
+
